ce35c33ba72e4c203ed7de903e665d863d1e5f68
andrew-velox <mohabbat.bd2020@gmail.com>
2025-06-24 02:11:08 +0600

    some built in formFields part_1

--- project_5/first_app/forms.py
+++ project_5/first_app/forms.py
@@ -3,3 +3,9 @@ from django import forms
 class makeForm(forms.Form):
     name = forms.CharField(label="UserName")
     email = forms.EmailField()
+    age = forms.IntegerField()
+    weight = forms.FloatField()
+    balance = forms.DecimalField()
+    check = forms.BooleanField()
+    birtday = forms.DateField()
+    appointment = forms.DateTimeField()
