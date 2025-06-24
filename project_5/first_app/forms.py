1f1a43c31a8a0db3bc2a3282cc526ab031eac8fd
andrew-velox <mohabbat.bd2020@gmail.com>
2025-06-24 14:57:55 +0600

    Buildin Form Validators

--- project_5/first_app/forms.py
+++ project_5/first_app/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core import validators
+
 
 class makeForm(forms.Form):
     name = forms.CharField(label="UserName",help_text="length will be less then 50 characters", required=False, widget=forms.Textarea(attrs={'id':"text_area_",'placeholder':'Your Name'}))
@@ -19,39 +21,54 @@ class makeForm(forms.Form):
     ITMS = [("P","Pepperoni"),("M","Mashroom"),("B","Beef")]
     pizza = forms.MultipleChoiceField(choices=ITMS,widget=forms.CheckboxSelectMultiple)
 
+# class studentF(forms.Form):
+#     name = forms.CharField(widget=forms.TextInput)
+#     email = forms.CharField(widget=forms.EmailInput)
+
+#     # def clean_name(self):
+#     #     valName = self.cleaned_data['name']
+
+#     #     if len(valName) < 5:
+#     #         raise forms.ValidationError("Name should be more then 5 characters")
+#     #     else:
+#     #         return valName
+        
+#     # def clean_email(self):
+#     #     valEmail = self.cleaned_data['email']
+
+#     #     if '@gmail' not in valEmail:
+#     #         raise forms.ValidationError("Email Must have @gmail")
+#     #     else:
+#     #         return valEmail
+
+
+#     def clean(self):
+#         clean_data = super().clean()
+#         valName = self.cleaned_data['name']
+#         valEmail = self.cleaned_data['email']
+
+#         if len(valName) < 5:
+#             raise forms.ValidationError("Name should be more then 5 characters")
+        
+#         if '@gmail' not in valEmail:
+#             raise forms.ValidationError("Email Must have @gmail")
+
+
+
+        
+def len_chk(text):
+
+    if len(text) < 10:
+        raise forms.ValidationError("Value must be at least 10 Characters")
+
 class studentF(forms.Form):
-    name = forms.CharField(widget=forms.TextInput)
-    email = forms.CharField(widget=forms.EmailInput)
+    name = forms.CharField(widget=forms.TextInput,validators=[validators.MinLengthValidator(5,message="Name should be more then 5 characters")])
 
-    # def clean_name(self):
-    #     valName = self.cleaned_data['name']
+    text = forms.CharField(widget=forms.TextInput,validators=[len_chk])
 
-    #     if len(valName) < 5:
-    #         raise forms.ValidationError("Name should be more then 5 characters")
-    #     else:
-    #         return valName
-        
-    # def clean_email(self):
-    #     valEmail = self.cleaned_data['email']
+    email = forms.CharField(widget=forms.EmailInput, validators=[validators.EmailValidator(message="Inter a Valid email")])
+    age = forms.IntegerField(validators=[validators.MaxValueValidator(34, message="Age must be less then 34"), validators.MinValueValidator(24, message="age must be greater then 24")])
+    file = forms.FileField(validators=[validators.FileExtensionValidator(allowed_extensions=["pdf"])])
 
-    #     if '@gmail' not in valEmail:
-    #         raise forms.ValidationError("Email Must have @gmail")
-    #     else:
-    #         return valEmail
-
-
-    def clean(self):
-        clean_data = super().clean()
-        valName = self.cleaned_data['name']
-        valEmail = self.cleaned_data['email']
-
-        if len(valName) < 5:
-            raise forms.ValidationError("Name should be more then 5 characters")
-        
-        if '@gmail' not in valEmail:
-            raise forms.ValidationError("Email Must have @gmail")
-
-
-
-        
+    
         
