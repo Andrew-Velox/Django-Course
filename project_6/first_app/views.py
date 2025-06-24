d02c3671278d80d3c39c9563e9f56667109bc574
andrew-velox <mohabbat.bd2020@gmail.com>
2025-06-25 01:24:34 +0600

    Deleting Model Data From Database

--- project_6/first_app/views.py
+++ project_6/first_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . import models
 # Create your views here.
 def home(request):
@@ -6,3 +6,9 @@ def home(request):
 
     print(students)
     return render(request,"first_app/home.html", {'data':students})
+
+def delete_student(request,roll):
+    std = models.Student.objects.get(pk=roll).delete()
+    print(std)
+
+    return redirect("homepage")
