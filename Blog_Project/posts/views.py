aab0e9992822e50819280451511a53fe8051057d
andrew-velox <mohabbat.bd2020@gmail.com>
2025-06-26 13:13:14 +0600

    Editing Post in Frontend

--- Blog_Project/posts/views.py
+++ Blog_Project/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from . import forms
+from . import forms,models
 # Create your views here.
 def add_post(request):
     if request.method =="POST":
@@ -11,4 +11,17 @@ def add_post(request):
     else:
         post_form=forms.PostForm()
 
+    return render(request, 'add_post.html',{'form':post_form})
+
+
+def edit_post(request,id):
+    post = models.Post.objects.get(pk=id)
+    post_form=forms.PostForm(instance=post)
+    # print(post.title)
+    if request.method =="POST":
+        post_form=forms.PostForm(request.POST,instance=post)
+
+        if post_form.is_valid():
+            post_form.save()
+            return redirect("homepage")
     return render(request, 'add_post.html',{'form':post_form})
