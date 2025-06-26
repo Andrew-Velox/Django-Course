0834c0098d8ce386c92f1a9b0ecba8eeb7c70fd4
andrew-velox <mohabbat.bd2020@gmail.com>
2025-06-26 20:18:05 +0600

    Some Extra Model Form

--- practice_day_3/first_app/models.py
+++ practice_day_3/first_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class MakeModelForm(models.Model):
+    # name = models.CharField(max_length=70,default="Dm")
+    # roll = models.IntegerField(primary_key=True,default=1)
+    # address = models.TextField(default="Dhaka")
+    # father_name = models.TextField(default="Velox")
+    # auto_field = models.AutoField(primary_key=True) 
+    # big_auto_field = models.BigAutoField(primary_key=True)
+
+    big_integer_field = models.BigIntegerField()
+    binary_field = models.BinaryField()
+    boolean_field = models.BooleanField()
+    char_field = models.CharField(max_length=255)
+    date_field = models.DateField()
+    date_time_field = models.DateTimeField()
+    decimal_field = models.DecimalField(max_digits=5, decimal_places=2)
+    duration_field = models.DurationField()
+    email_field = models.EmailField()
+    file_field = models.FileField(upload_to='files/')
+    generic_ip_address_field = models.GenericIPAddressField()
+    integer_field = models.IntegerField()
+    url_field = models.URLField()
+
+    
+
+    # def __str__(self):
+    #     return f"{self.name}"
--- practice_day_3/first_app/views.py
+++ practice_day_3/first_app/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from . forms import MakeForm
+from . models import MakeModelForm
+# from . import models
 # Create your views here.
 
 
 def home(request):
-    if request.method=="POST":
-        form = MakeForm(request.POST)
+    #---------this is for the form fileds
+    # if request.method=="POST":
+    #     form = MakeForm(request.POST)
 
-        if form.is_valid():
-            # print(form.cleaned_data)
-            return render(request,'home.html',{"form":form})
-    else:
-        form= MakeForm()
-    return render(request,'home.html',{"form":form})
+    #     if form.is_valid():
+    #         # print(form.cleaned_data)
+    #         return render(request,'home.html',{"form":form})
+    # else:
+    #     form= MakeForm()
+
+    # ---------------this is for the Model Form-------
+    data = MakeModelForm.objects.all()
+    print(data)
+    return render(request,'home.html',{"form":data})
