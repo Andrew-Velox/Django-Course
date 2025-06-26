6e4a1370dc90e5ecd2a1ea8dac3b00aa92bf08d7
andrew-velox <mohabbat.bd2020@gmail.com>
2025-06-26 13:20:15 +0600

    Delete Post in Frontend

--- Blog_Project/posts/views.py
+++ Blog_Project/posts/views.py
@@ -25,3 +25,11 @@ def edit_post(request,id):
             post_form.save()
             return redirect("homepage")
     return render(request, 'add_post.html',{'form':post_form})
+
+
+
+def delete_post(request,id):
+    post = models.Post.objects.get(pk=id)
+    post.delete()
+    return redirect("homepage")
+    
